34e509453788b809b4df02f060572077dfba0e16
UlfetKacay <ukacay87@gmail.com>
2021-05-31 10:42:42 +0300

    permütasyon

---
+++ permütasyon.py
@@ -0,0 +1,7 @@
+num = [1,2,3]
+num_set = set(num)
+solution = [[]]
+for index in range(len(num)) :
+    solution = [i + [j] for i in solution for j in num_set.difference(set(i))]
+    print(solution)
+solution
